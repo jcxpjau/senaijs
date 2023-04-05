3ec249ae70f393f89f2604d7ef9ae2d2c3bc00ba
Julio Xavier <jcxpjau@live.com>
2023-04-05 12:32:48 -0300

    soma adicionada

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 document.addEventListener( "click" , showcolor );
 
 document.getElementById( "botao" ).addEventListener( "click" , exibenome );
+document.getElementById( "btn-somar" ).addEventListener( "click" , soma );
 
 function showcolor()
 {
@@ -12,5 +13,13 @@ function exibenome()
     let usuario = document.getElementById( "nome" ).value;    
     document.getElementById( "mensagem" ).innerHTML = "Meu nome é: " + usuario;
     document.getElementById( "nome" ).value = "";
+}
+function soma()
+{
+    let num1 = document.getElementById( "num1" ).value;
+    let num2 = document.getElementById( "num2" ).value; 
+    document.getElementById( "resultado-soma" ).innerHTML = parseFloat( num1 ) + parseFloat( num2 );
+    document.getElementById( "num1" ).value = "";
+    document.getElementById( "num2" ).value = "";
 }
 
